65cf3ec39a8d19b13dd6b5aa4e2ec056ed40c510
Luiz Zimmermann <luizzimmermann@edu.univali.br>
2020-09-07 14:52:09 -0300

    Implementando o método Hamming

--- Trabalho1_M1/main.py
+++ Trabalho1_M1/main.py
@@ -1,16 +1,102 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+def check_pos(pos, tam):
+
+    for i in range(tam):
+
+        if 2**i == pos:
+            return True
+
+    return False
+
+def format_vet(vet,p):
+    # alterar o tamanho do aux
+    aux = np.zeros(12)
+    for i in range(1,len(aux)+1):
+        # alterar o segundo valor
+        if check_pos(i, 4):
+            #print("p: ", p)
+            v = p[0]
+            p = np.delete(p,0)
+            aux[i-1] = v
+
+        else:
+            v = vet[0]
+            vet = np.delete(vet,0)
+            aux[i-1] = v
 
 
-# Press the green button in the gutter to run the script.
+    return aux
+
+def redundant_values(i, vet):
+    aux = []
+    x = 0
+    y = 2**i
+
+    for j in range((2**i)-1,len(vet)):
+        if x < 2**i:
+            aux.append(vet[j])
+            x += 1
+        elif y > 1:
+            y -= 1
+        else:
+            x = 0
+            y = 2**i
+
+    return aux
+
+def encode(vet):
+    # alterar quantidade de bits paridade
+    p = np.zeros(4)
+    vet = format_vet(vet,p)
+
+    for i in range(len(p)):
+        aux = redundant_values(i, vet)
+        #print("i: ",i, aux)
+        for j in aux[1:len(aux)]:
+            p[i] = int(p[i])^int(j)
+
+    for i in range(1,len(vet)+1):
+        if check_pos(i, 4):
+            v = p[0]
+            p = p[1:len(p)]
+            vet[i-1] = v
+
+    return vet
+
+
+def decode(vet):
+    # alterar quantidade de bits paridade
+    k = np.zeros(4)
+
+    for i in range(len(k)):
+        aux = redundant_values(i, vet)
+        for j in aux:
+            #print("i: ",i, "valores: ",int(k[i]),int(j),int(k[i])^int(j))
+            k[i] = int(k[i])^int(j)
+
+    print(k)
+    if k.__contains__(1):
+        pos = 0
+        for i in range(len(k)):
+            if k[i] == 1:
+                pos+=2**i
+
+        print("há erro na posição: ",pos)
+        print("Mensagem com erro: ", vet)
+        vet[pos-1] = 0
+        print("Mensagem corrigida: ", vet)
+
+    else:
+        print("Não há erro")
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    vet = np.array([1,0,1,0,1,0,1,0])
+    #vet = np.array([1,0,0,1])
+    vet = encode(vet)
+    vet[9] = 1
+    decode(vet)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
