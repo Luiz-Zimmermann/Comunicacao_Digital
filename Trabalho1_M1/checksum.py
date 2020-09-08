aac51b7d59aabc4cb74c16af8328e5106e3cfe78
diogomarchi <42180006+diogomarchi@users.noreply.github.com>
2020-09-08 16:15:24 -0300

    implementado checksum

--- Trabalho1_M1/checksum.py
+++ Trabalho1_M1/checksum.py
@@ -1,5 +1,14 @@
 import numpy as np
 
+def comp1(palavra):
+    aux = np.zeros(len(palavra),dtype=int)
+    for i in range(len(palavra)):
+        if palavra[i] == 0:
+            aux[i] = 1
+        else:
+            aux[i] = 0
+    return aux
+
 def somaUm(x3, x4):
 
     result2 = np.zeros(len(x3), dtype=int)
@@ -76,11 +85,52 @@ def monta_palavra(palavra1, palavra2, palavra3, palavra4):
     print("Primeira palavra", resultado2)
     print("Segunda palavra ", palavra4)
     resultado3 = soma(resultado2, palavra4)
-    print("terceira soma: ", resultado3, "\n\n")
+    print("terceira soma: ", resultado3, "\n")
 
-    return resultado3
+    EDC = comp1(resultado3)
+    print("EDC: ", EDC, "\n")
+
+    Tx = str(palavra1) + " " + str(palavra2) + " " + str(palavra3) + " " + str(palavra4) + ":" + str(EDC)
+    print("Tx", Tx)
+    return EDC
+
+def verificaErro(palavra1, palavra2, palavra3, palavra4, EDC):
+    print("Montando a primeira soma...")
+    print("Primeira palavra", palavra1)
+    print("Segunda palavra ", palavra2)
+    resultado1 = soma(palavra1, palavra2)
+    print("primeira soma: ", resultado1, "\n\n")
+
+    print("Montando a segunda soma...")
+    print("Primeira palavra", resultado1)
+    print("Segunda palavra ", palavra3)
+    resultado2 = soma(resultado1, palavra3)
+    print("segunda soma: ", resultado2, "\n\n")
+
+    print("Montando a terceira soma...")
+    print("Primeira palavra", resultado2)
+    print("Segunda palavra ", palavra4)
+    resultado3 = soma(resultado2, palavra4)
+    print("terceira soma: ", resultado3, "\n")
+
+    resultadoFinal = soma(resultado3, EDC)
+    print("resultado final: ", resultadoFinal, "\n")
+
+    Rx = str(palavra1) + " " + str(palavra2) + " " + str(palavra3) + " " + str(palavra4) + ":" + str(EDC)
+    print("Rx", Rx)
+    return resultadoFinal
 
 
-checksum = monta_palavra(np.asarray([1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0], dtype=int), np.asarray([0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1], dtype=int), np.asarray([0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1], dtype=int), np.asarray([1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0], dtype=int))
+EDC = monta_palavra(np.asarray([1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0], dtype=int), np.asarray([0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1], dtype=int), np.asarray([0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1], dtype=int), np.asarray([1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0], dtype=int))
 
-print("checksum final: ", checksum)
+VerificaErroFinal = verificaErro(np.asarray([1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0], dtype=int), np.asarray([0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1], dtype=int), np.asarray([0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1], dtype=int), np.asarray([1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0], dtype=int), EDC)
+
+testa = 0
+for i in range(len(VerificaErroFinal)):
+    if VerificaErroFinal[i] == 0:
+        testa+=1
+
+if testa == 0:
+    print("deu bom")
+else:
+    print("deu ruim")
